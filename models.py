258e3e721628c874f0b51b246bf1f66957b7af44
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-07-30 14:06:00 +0330

    basic data model in python

---
+++ models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+ 
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+   
