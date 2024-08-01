540852451952fe5af9e7f105ee25854edb724812
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-02 01:35:34 +0330

    add register, captcha, email sender

--- web/models.py
+++ web/models.py
@@ -1,6 +1,31 @@
 from __future__ import unicode_literals
+from django.utils.crypto import get_random_string
 from django.db import models
+from django.contrib.auth.models import User
 
+
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    date = models.DateTimeField()
+    def __unicode__(self):
+        return self.title
+
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)  
+
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __unicode__(self):
+        return "{}_token".format(self.user)
 
 
 class Expense(models.Model):
@@ -12,13 +37,6 @@ class Expense(models.Model):
     def __unicode__(self):
         return "{}-{}-{}".format(self.date, self.user, self.amount)
 
-
-class Token(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=48)
-
-    def __unicode__(self):
-        return "{}_token".format(self.user)
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
--- web/urls.py
+++ web/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     url(r'^edit/expense/?$', views.edit_expense, name='edit_expense'),
     url(r'^submit/income/?$', views.submit_income, name='submit_income'),
     url(r'^edit/income/?$', views.edit_income, name='edit_income'),
+    url(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
     url(r'^q/incomes/?$', views.query_incomes, name='query_incomes'),
     url(r'^q/expenses/?$', views.query_expenses, name='query_expenses'),
     url(r'^accounts/register/?$', views.register, name='register'),
     url(r'^accounts/whoami/?$', views.whoami, name='whoami'),
     url(r'^accounts/login/?$', views.login, name='login'),
+    url(r'^news/?$', views.news, name='news'),
+    url(r'^$', views.index, name='index'),
 ]
