f4270d0f838a5a609df7ed7b767f844c63691ff8
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-01 17:03:40 +0330

    add another webservice

---
+++ web/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
+    url(r'^edit/expense/?$', views.edit_expense, name='edit_expense'),
+    url(r'^submit/income/?$', views.submit_income, name='submit_income'),
+    url(r'^edit/income/?$', views.edit_income, name='edit_income'),
+    url(r'^q/incomes/?$', views.query_incomes, name='query_incomes'),
+    url(r'^q/expenses/?$', views.query_expenses, name='query_expenses'),
+    url(r'^accounts/register/?$', views.register, name='register'),
+    url(r'^accounts/whoami/?$', views.whoami, name='whoami'),
+    url(r'^accounts/login/?$', views.login, name='login'),
+]
